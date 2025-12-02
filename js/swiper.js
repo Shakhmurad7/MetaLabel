eadfcdb8fed27f05f023920753d3069fd5d99bb1
Shakhmurad <muradb356@gmail.com>
2025-12-02 21:42:13 +0400

    add swiper

---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  loop: true,
+  freeMode: true,
+  freeModeMomentum: false,
+  slidesPerView: 2,
+  speed:3000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false
+  },
+  allowTouchMove: false
+});
